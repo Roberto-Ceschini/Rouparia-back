359569c699d0f593990182b72964a130eeda380e
Roberto-Ceschini <robert17ceschini@gmail.com>
2025-03-01 16:07:55 -0300

    Hotfix modules entidades
    Foi adiconado o PrismaService no provider Module das entidades

--- src/funcionario_rouparia/funcionario_rouparia.module.ts
+++ src/funcionario_rouparia/funcionario_rouparia.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FuncionarioRoupariaService } from './funcionario_rouparia.service';
 import { FuncionarioRoupariaController } from './funcionario_rouparia.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [FuncionarioRoupariaController],
-  providers: [FuncionarioRoupariaService],
+  providers: [FuncionarioRoupariaService, PrismaService],
 })
 export class FuncionarioRoupariaModule {}
